1fb041bc77bbf0b525b1e008fe30192238931083
NRT666 <n.rejas3@gmail.com>
2016-07-28 20:09:21 -0400

    Add files via upload

---
+++ NRT6662789.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int N = 0;
+            int[] vec = new int[3];
+            for(int i = 0; i<S.Length; i = i+2)
+            { 
+                N = (int) (S[i]-48);
+                vec[N - 1] = vec[N - 1] + 1;
+            }
+
+            for (int i = 0; i < vec[0]; i++)
+            {
+                if (i == vec[0] - 1 && vec[1] == 0 && vec[2] == 0)
+                {
+                    Console.Write(1);
+                    break;
+                }
+                Console.Write(1+"+");
+            }
+            for (int i = 0; i < vec[1]; i++)
+            {
+                if (i == vec[1] - 1 && vec[2] == 0)
+                {
+                    Console.Write(2);
+                    break;
+                }
+                Console.Write(2 + "+");
+                
+            }
+            for (int i = 0; i < vec[2]; i++)
+            {
+                if (i == vec[2] - 1)
+                {
+                    Console.Write(3);
+                    break;
+                }
+                Console.Write(3 + "+");
+            }
+            Console.ReadKey();
+        }
+    }
+}
