6ceb61215cab6524a77a924ce53a5b6b1a0b09e4
NRT666 <n.rejas3@gmail.com>
2016-07-29 22:41:14 -0400

    Add files via upload

---
+++ NRT6663375.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phrases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string S;
+            int j =0;
+            int X = 0;
+            int[] vec = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                S = Console.ReadLine();
+                for(j =0; j<S.Length/2; j++)
+                {
+                    if(S[j]!=S[(S.Length/2)+j+1])
+                    {
+                        X = X + 1;  
+                    }
+                }
+                vec[i] = X;
+                X = 0;
+            }
+            for (j = 0; j < N; j++)
+            {
+                Console.WriteLine(vec[j]);
+            }
+                Console.ReadKey();
+        }
+    }
+}
