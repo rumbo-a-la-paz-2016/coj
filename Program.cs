70ee6377a394e1f13a7552c71e8f0833409c3394
Alancq <netelmer@gmail.com>
2016-07-29 23:36:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1840ALAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= N; i++) 
+            {
+                string m = Console.ReadLine();
+                int b = 0, r = 0, o = 0, k = 0, e = 0, n = 0,  P = 0;
+                for ( int u = 0; u < m.Length; u++)
+                {
+                    if (m[u]=='B')
+                    {
+                        b++;
+                    }
+                    else if (m[u]=='R')
+                    {
+                        r++;
+                    }
+                    else  if (m[u]=='O')
+                    {
+                        o++;
+                    }
+                    else if (m[u]=='K')
+                    {
+                        k++;
+                    }
+                    else if (m[u]=='E')
+                    {
+                        e++;
+                    }
+                    else if (m[u]=='N')
+                    {
+                        n++;
+                    }
+                    
+                }
+                P = b;
+                if (b==P && r==P && o==P && k==P && e==P && n==P)
+                {
+                    Console.WriteLine("No Secure");
+                }
+                else
+                {
+                    Console.WriteLine("Secure");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
